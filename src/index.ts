6a2fd9251a55588bd1bcacd6c46af9f0b8ed8e57
Andika Bahari <kzlfxx@gmail.com>
2021-08-22 06:32:24 +0700

    Setup web server.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import express from "express";
+import path from "path";
+
+const app = express();
+const port: number = 5000;
+
+app.get("/", (req, res) => {
+  const file: string = path.join(__dirname, "pages", "index.html");
+  res.sendFile(file);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
